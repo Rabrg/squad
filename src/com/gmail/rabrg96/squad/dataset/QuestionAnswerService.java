8d5ff160409e40d8625f2551cc5d5ab34c86b826
Ryan Greene <rabrg96@gmail.com>
2016-07-10 00:33:08 -0500

    Throw exception

--- src/com/gmail/rabrg96/squad/dataset/QuestionAnswerService.java
+++ src/com/gmail/rabrg96/squad/dataset/QuestionAnswerService.java
@@ -1,6 +1,5 @@
 package com.gmail.rabrg96.squad.dataset;
 
-import edu.stanford.nlp.ling.IndexedWord;
 import edu.stanford.nlp.semgraph.SemanticGraph;
 import edu.stanford.nlp.semgraph.SemanticGraphEdge;
 import edu.stanford.nlp.simple.Sentence;
@@ -32,48 +31,49 @@ public final class QuestionAnswerService {
             "how", "why", "where", "can", "will", "could", "should", "would", "is", "are", "am", "shall", "may", "did",
             "do", "does");
 
+    // TODO: enum?
     public String getQuestionType() {
-        if (questionType == null) { // TODO: enum?
-            final Sentence sentence = new Sentence(question);
+        if (questionType != null)
+            return questionType;
 
-            // Look for question types as first to words and if so use the first
-            final List<String> words = sentence.words();
-            final String first = words.get(0).toLowerCase();
-            final String second = words.get(1).toLowerCase();
-            if (QUESTION_TYPES.contains(first) && QUESTION_TYPES.contains(second))
-                return (questionType = first);
+        final Sentence sentence = new Sentence(question);
 
-            // Check if last word is question type
-            final String last = Character.isLetterOrDigit(words.get(words.size() - 1).charAt(0))
-                    ? words.get(words.size() - 1).toLowerCase() : words.get(words.size() - 2).toLowerCase();
-            if (QUESTION_TYPES.contains(last))
-                return (questionType = last);
+        // Look for question types as first to words and if so use the first
+        final List<String> words = sentence.words();
+        final String first = words.get(0).toLowerCase();
+        final String second = words.get(1).toLowerCase();
+        if (QUESTION_TYPES.contains(first) && QUESTION_TYPES.contains(second))
+            return (questionType = first);
 
-            // If not begin with two question words, use dependencies
-            final SemanticGraph graph = sentence.dependencyGraph();
-            for (final SemanticGraphEdge edge : graph.edgeListSorted()) {
-                final String source = edge.getSource().word().toLowerCase();
-                final String target = edge.getTarget().word().toLowerCase();
-                final String type = QUESTION_TYPES.contains(target) ? target : source; // TODO: randomly gave target priority over source
-                if (QUESTION_TYPES.contains(type)) {
-                    questionType = type;
+        // Check if last word is question type
+        final String last = Character.isLetterOrDigit(words.get(words.size() - 1).charAt(0))
+                ? words.get(words.size() - 1).toLowerCase() : words.get(words.size() - 2).toLowerCase();
+        if (QUESTION_TYPES.contains(last))
+            return (questionType = last);
 
-                    // Debug types which are prone to be error
-                    switch (type) {
-                        case "do":
-                        case "is":
-                        case "are":
-                            System.out.println("Question: " + question);
-                            System.out.println("Detected type: " + type);
-                            System.out.println("Edge list: " + graph.edgeListSorted());
-                            System.out.println("=====");
-                            break;
-                    }
-                    break;
+        // If not begin with two question words, use dependencies
+        final SemanticGraph graph = sentence.dependencyGraph();
+        for (final SemanticGraphEdge edge : graph.edgeListSorted()) {
+            final String source = edge.getSource().word().toLowerCase();
+            final String target = edge.getTarget().word().toLowerCase();
+            final String type = QUESTION_TYPES.contains(target) ? target : source; // TODO: randomly gave target priority over source
+
+            if (QUESTION_TYPES.contains(type)) {
+                // Debug types which are prone to be error
+                switch (type) {
+                    case "do":
+                    case "is":
+                    case "are":
+                        System.out.println("Question: " + question);
+                        System.out.println("Detected type: " + type);
+                        System.out.println("Edge list: " + graph.edgeListSorted());
+                        System.out.println("=====");
+                        break;
                 }
+                return (questionType = type);
             }
         }
-        return questionType;
+        throw new IllegalStateException("No question type detected for sentence \"" + question + "\"");
     }
 
     @Override
