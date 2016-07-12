a7522faf70bbea1094c97dcdf34ffee0a262c7ed
Ryan Greene <rabrg96@gmail.com>
2016-07-12 16:26:15 -0500

    Stuff

--- src/com/gmail/rabrg96/squad/dataset/Paragraph.java
+++ src/com/gmail/rabrg96/squad/dataset/Paragraph.java
@@ -3,7 +3,7 @@ package com.gmail.rabrg96.squad.dataset;
 import edu.stanford.nlp.simple.Document;
 import edu.stanford.nlp.simple.Sentence;
 
-import java.util.List;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public final class Paragraph {
@@ -27,6 +27,42 @@ public final class Paragraph {
             contextSentences = document.sentences().stream().map(Sentence::text).collect(Collectors.toList());
         }
         return contextSentences;
+    }
+
+    // answer is in the first or second sentence over 70% of the time
+    // could be improved by ignoring (or weighting) common words
+    public Map<String, Integer> getRelevancyOrederedContextSentences(final QuestionAnswerService qas) {
+        final Map<String, Integer> relevancyOrdered = new HashMap<>(getContextSentences().size());
+        final Sentence questionSentence = new Sentence(qas.getQuestion());
+        for (final String context : getContextSentences()) {
+            final Sentence contextSentence = new Sentence(context);
+            int sentenceCounter = 0;
+            for (final String contextWord : contextSentence.lemmas()) {
+                for (final String questionWord : questionSentence.lemmas()) {
+                    if (contextWord.toLowerCase().equals(questionWord.toLowerCase())) {
+                        sentenceCounter++;
+                    }
+                }
+            }
+            relevancyOrdered.put(context, sentenceCounter);
+        }
+        sortByValue(relevancyOrdered);
+        return relevancyOrdered;
+    }
+
+    private static <K, V extends Comparable<? super V>> Map<K, V> sortByValue(Map<K, V> map) {
+        List<Map.Entry<K, V>> list = new LinkedList<Map.Entry<K, V>>(map.entrySet());
+        Collections.sort(list, new Comparator<Map.Entry<K, V>>() {
+            public int compare(Map.Entry<K, V> o1, Map.Entry<K, V> o2) {
+                return (o1.getValue()).compareTo(o2.getValue());
+            }
+        });
+
+        Map<K, V> result = new LinkedHashMap<K, V>();
+        for (Map.Entry<K, V> entry : list) {
+            result.put(entry.getKey(), entry.getValue());
+        }
+        return result;
     }
 
     @Override
--- test/com/gmail/rabrg96/squad/dataset/DatasetTest.java
+++ test/com/gmail/rabrg96/squad/dataset/DatasetTest.java
@@ -2,8 +2,18 @@ package com.gmail.rabrg96.squad.dataset;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import edu.stanford.nlp.hcoref.CorefCoreAnnotations;
+import edu.stanford.nlp.hcoref.data.CorefChain;
+import edu.stanford.nlp.ling.CoreAnnotations;
+import edu.stanford.nlp.ling.CoreLabel;
+import edu.stanford.nlp.pipeline.Annotation;
+import edu.stanford.nlp.pipeline.StanfordCoreNLP;
+import edu.stanford.nlp.simple.Sentence;
 
 import java.io.FileReader;
+import java.util.List;
+import java.util.Map;
+import java.util.Properties;
 
 public class DatasetTest {
 
@@ -12,26 +22,78 @@ public class DatasetTest {
             final Gson gson = new GsonBuilder().create();
             final Dataset dataset = gson.fromJson(new FileReader("./res/dev-v1.0.json"), Dataset.class);
 
-            final long start = System.currentTimeMillis();
-            int articles = 0, paragraphs = 0, questions = 0;
-            for (final Article article : dataset.getData()) {
-                articles++;
-                for (final Paragraph paragraph : article.getParagraphs()) {
-                    paragraphs++;
-                    for (final QuestionAnswerService qas : paragraph.getQas()) {
-                        questions++;
-                        try {
-                            if (qas.isMultiplePossibleType()) {
-                                System.out.println(qas.getQuestionType() + ": " + qas.getQuestion());
-                            }
-                        } catch (final IllegalStateException e) {
-//                            System.out.println("Failed to detect type: " + qas.getQuestion());
-                        }
-                    }
+            Properties props = new Properties();
+            props.put("annotators", "tokenize, ssplit, pos, lemma, ner, parse, dcoref");
+            StanfordCoreNLP pipeline = new StanfordCoreNLP(props);
+
+
+            for (final Paragraph paragraph : dataset.getData().get(0).getParagraphs()) {
+                String text = paragraph.getContext();
+                Annotation document = new Annotation(text);
+                pipeline.annotate(document);
+
+                Map<Integer, CorefChain> graph = document.get(CorefCoreAnnotations.CorefChainAnnotation.class);
+                System.out.println("Graph: " + graph.toString());
+                for (Map.Entry<Integer, CorefChain> entry : graph.entrySet()) {
+                    CorefChain chain = entry.getValue();
+                    CorefChain.CorefMention repMention = chain.getRepresentativeMention();
+                    System.out.println("Chain: " + chain.toString());
+                    System.out.println("Rep: " + repMention.toString());
                 }
+//                for (final String text : paragraph.getContextSentences()) {
+//                    final Sentence sentence = new Sentence(text);
+//                    final List<String> words = sentence.words();
+//                    final List<String> nerTags = sentence.nerTags(); // TODO: verify 7 tag
+//                    System.out.print("CONTEXT SENTENCE: ");
+//                    for (int i = 0; i < words.size(); i++) {
+//                        System.out.print(words.get(i) + " ");
+//                        if (!"O".equals(nerTags.get(i))) {
+//                            System.out.print("(" + nerTags.get(i) + ") ");
+//                        }
+//                    }
+//                    System.out.println();
+//                }
+
+//                for (final QuestionAnswerService qas : paragraph.getQas()) {
+//                    if ("who".equals(qas.getQuestionType())) {
+//                        System.out.println("QUESTION: " + qas.getQuestion());
+//                        System.out.println("ANSWER: \"" + qas.getAnswers().get(0).getText() + "\"");
+//                        int i = 0;
+//                        for (final String context : paragraph.getRelevancyOrederedContextSentences(qas).keySet()) {
+//                            if (context.contains(qas.getAnswers().get(0).getText())) {
+//                                System.out.println("Detected in sentence: " + i);
+//                                break;
+//                            }
+//                            i++;
+//                        }
+//                        System.out.println();
+//                    }
+//                }
             }
-            final long elapsed = (System.currentTimeMillis() - start);
-            System.out.println("Parsed " + questions + " questions in " + elapsed + "ms (" + (((double) elapsed) / questions) + "ms/q)");
+//            final long start = System.currentTimeMillis();
+//            int articles = 0, paragraphs = 0, questions = 0;
+//            for (final Article article : dataset.getData()) {
+//                articles++;
+//                for (final Paragraph paragraph : article.getParagraphs()) {
+//                    paragraphs++;
+//                    for (final String sentenceText : paragraph.getContextSentences()) {
+//                        System.out.println(new Sentence(sentenceText).nerTags() + ": " + sentenceText);
+//                    }
+//                    for (final QuestionAnswerService qas : paragraph.getQas()) {
+//                        questions++;
+//                        try {
+//                            if ("who".equals(qas.getQuestionType())) {
+//                                System.out.println(qas.getAnswers() + ": " + qas.getQuestion());
+//                            }
+//                        } catch (final IllegalStateException e) {
+////                            System.out.println("Failed to detect type: " + qas.getQuestion());
+//                        }
+//                    }
+//                    break; // one paragraph
+//                }
+//            }
+//            final long elapsed = (System.currentTimeMillis() - start);
+//            System.out.println("Parsed " + questions + " questions in " + elapsed + "ms (" + (((double) elapsed) / questions) + "ms/q)");
         } catch (final Exception e) {
             e.printStackTrace();
         }
